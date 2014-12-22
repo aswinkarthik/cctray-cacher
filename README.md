1ce230c4a395779e28a07750722a6621e76a6b8e
Aswin Karthik S <aswinks@thoughtworks.com>
2014-12-22 23:38:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CCTray Cacher
+
+Caches the cctray.xml for a CI server and serves it from a local server
+
+## Requirements
+- Ruby (Uses WEBBrick server)
+- Set environment variable ```$CI_MASTER_FEED_URL``` to the url of the cctray.xml 
+
+## Usage
+```bash run.sh```
---
+++ run.sh
@@ -0,0 +1,35 @@
+set -e
+
+## Checking if env is set
+if [ -z $CI_MASTER_FEED_URL ]
+then
+echo 'Set $CI_MASTER_FEED_URL'
+exit 1
+fi
+
+## Creating directory for xml to host if it does not exist
+mkdir -p data
+cd data
+
+#Hosting server
+ruby -run -e httpd . -p 9095 &
+
+control_c()
+## run if user hits control-c
+{
+	echo -en "\n***Exiting at `date`***\n"
+	pkill -P $$	
+	exit $?	
+}
+
+## Trapping keyboard interrupt
+trap control_c SIGINT
+
+## Looping calls
+while true
+do
+echo "Getting XML at `date`"
+curl $CI_MASTER_FEED_URL > ./cctray.xml 2>/dev/null 
+sleep 3
+done
+
